e84f017dc905fbc6a2fc10e1fa10041526cf82e5
Vanya Startsev <vanya.startsev.60@mail.ru>
2022-02-20 19:52:53 +0500

    Цвет каюты

--- main.py
+++ main.py
@@ -51,6 +51,26 @@ def distribution():
     return render_template('rooms.html', news=news_list)
 
 
+@app.route('/table/<string:sex>/<int:old>')
+def table(sex, old):
+    if sex == 'female':
+        if old < 21:
+            img = "https://wonder-day.com/wp-content/uploads/2021/02/wonder-day-monster-58.jpg"
+            color = "https://images-ext-1.discordapp.net/external/bmBHS6bHrPx9_M-fn1iiNFJBkKKKnC87XGuN4SGRsFs/https/klike.net/uploads/posts/2019-11/1572688384_10.jpg?width=993&height=745"
+        else:
+            img = "https://raskrasil.com/wp-content/uploads/Raskrasil.com-Alien-7.jpg"
+            color = "http://geo-storm.ru/upload/information_system_17/1/7/5/item_1758/information_items_1758.jpg"
+    else:
+        if old < 21:
+            img = "https://wonder-day.com/wp-content/uploads/2021/02/wonder-day-monster-58.jpg"
+            color = "https://images-ext-1.discordapp.net/external/uCQ1-sozLsvIWFRGU7gXJBxMyvsoNtcydiSNVE_TuH8/http/900igr.net/datai/tsvet-i-forma/TSveta-3.files/0007-013-Goluboj-tsvet.jpg"
+        else:
+            img = "https://raskrasil.com/wp-content/uploads/Raskrasil.com-Alien-7.jpg"
+            color = "https://images-ext-2.discordapp.net/external/21yMBeWc22frjuNZWaGyqz20UOPSi7vFQ4vEY8qwS08/https/klike.net/uploads/posts/2019-11/1572686545_7.jpg"
+    return render_template('table.html', img=img, img_color=color)
+
+
+
 class LoginForm(FlaskForm):
     astronaut_username = StringField('id астронавта', validators=[DataRequired()])
     astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
